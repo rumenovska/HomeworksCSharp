8be0c37a0e07fc390f50fe9c63946874ab41b8b6
Windows <Windows@WINDOWS-PC>
2019-03-20 14:37:41 +0100

    class8

---
+++ ExercisseClass8/ExercisseClass8/Classes/Cinema.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ExercisseClass8
+
+{
+    public class Cinema
+    {
+        public string Name { get; set; }
+        public List<int> Halls { get; set; }
+        public List<Movie> ListOfMovies { get; set; }
+
+        public void MoviePlaying(Movie movie)
+        {
+            Console.WriteLine($"Watching {movie.Title}");
+        }
+
+        public Cinema()
+        {
+            Halls = new List<int>();
+            ListOfMovies = new List<Movie>();
+        }
+        public Cinema(string name)
+        {
+            this.Name = name;
+            Halls = new List<int>();
+            ListOfMovies = new List<Movie>();
+        }
+    }
+}
---
+++ ExercisseClass8/ExercisseClass8/Classes/Movie.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisseClass8
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+        public int Rating { get; set; }
+        public int TicketPrice { get; set; }
+
+        public Movie(string title, Genre genre, int rating)
+        {
+            this.Title = title;
+            this.Genre = Genre;
+            try
+            {
+                if (rating >= 1 && rating <= 5)
+                {
+                    this.Rating = rating;
+
+                }
+                else
+                {
+                    throw new Exception("Rating should be a number from 1 to 5");
+                }
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                this.Rating = 1;
+                Console.WriteLine(ex.Message);
+            }
+
+            this.TicketPrice = 5 * rating;
+        }
+    }
+}
